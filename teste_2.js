2cbee3bf11167f87d7ae31325a95e2e2940f9562
JAIME <netto.jaime@gmail.com>
2023-01-29 19:41:43 -0300

    Alteração da forma de pesquisa, retornando valores correspondente

--- teste_2.js
+++ teste_2.js
@@ -1,39 +1,61 @@
 var codigo = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
-
+var vogais = ["a", "e", "i", "o", "u"];
+var conversao = ["ai", "enter", "imes", "ober", "ufat"];
 var input1 ="gato";
 var input2 ="gaitober";
 
-var letra = 0;
+var posicaoLetra = 3;
 
-//console.log(input1.length)
+console.log(input1[posicaoLetra])
+console.log(vogais.indexOf(input1[posicaoLetra]))
+
+if (vogais.indexOf(input1[posicaoLetra]) >= 0) {
+    console.log(conversao[vogais.indexOf(input1[posicaoLetra])])
+} else {
+    console.log(input1[posicaoLetra])
+}
 
 //console.log(codigo[0][1])
 
-while (letra < input1.length) {
+/*while (posicaoLetra < input1.length) {
 
-    var codigo = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
+    
+    //var codigo = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
+    var item = 0
 
     if (codigo.length != 0) {
 
-        for (var item = 0; item < codigo.length; item++) {
+        for (item; item < codigo.length; item++) {
+
             
-            if (input1[letra] == codigo[item][0]) {
+            
+            if (codigo.indexOf(input1[posicaoLetra]) == true) {
                 
                 console.log(codigo[item][1]);
-                break;
+                //break;
+            
+            } 
+            
+            if (input1[posicaoLetra] != codigo[item][0]) {
+                
+                console.log(input1[posicaoLetra]);
+        
             
             }
-        }
-            
-        if (input1[letra] != codigo[item][0]) {
 
-            console.log(input1[letra]);
-        
+            if (input1[posicaoLetra] != " ") {
+
+                console.log(" ");
+                break
+            }
+
         }
+        //console.log(codigo[item][item]);   
+        
        
     }
 
-    letra ++
+    posicaoLetra ++
     
-}
+}*/
 
